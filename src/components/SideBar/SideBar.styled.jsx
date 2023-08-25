4280e2bae74a2d27ee8bb0d2434421ec54fa6915
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-25 23:15:04 +0300

    Create SideBar.styled.jsx

---
+++ src/components/SideBar/SideBar.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  width: 0px;
+  height: 770px;
+  float: left;
+  background-color: #e6e6b1;
+
+  @media screen and (min-width: 1440px) {
+    width: 260px;
+  }
+`;

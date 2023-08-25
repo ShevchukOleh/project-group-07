4e24508506f0577a164b86989e28f5c54142b0ac
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-25 19:55:24 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,32 @@
-import { Header } from './AppBar.styled';
+import {
+  Header,
+  HeaderNav,
+  ChangeThemeBtn,
+  ThemeLabel,
+  ThemeIcon,
+  UserInfoBtn,
+  UserName,
+  UserAvatar,
+} from './AppBar.styled';
 
 export const AppBar = () => {
   return (
     <Header>
-    
+      <HeaderNav>
+        <ChangeThemeBtn>
+          <ThemeLabel>Theme</ThemeLabel>
+          <ThemeIcon width="16px" height="16px">
+            <use href="/src/images/symbol-defs.svg#icon-chevron"></use>
+          </ThemeIcon>
+        </ChangeThemeBtn>
+
+        <UserInfoBtn>
+          <UserName>User</UserName>
+          <UserAvatar>
+            <img src="" alt="User name"></img>
+          </UserAvatar>
+        </UserInfoBtn>
+      </HeaderNav>
     </Header>
   );
 };

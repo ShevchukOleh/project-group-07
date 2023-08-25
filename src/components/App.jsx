599d23f2066dc45a04b531e2c6dcf800d7f97240
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-25 12:46:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,8 @@
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+
 import { Layout } from './Layout';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}></Route>
-    </Routes>
+    <Layout/>
   );
 };

c28936b404a0140917cfb82d3a3b5ea956916c34
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-25 16:13:02 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Bars } from 'components/Bars';
 import { AppBar } from 'components/AppBar';
+import { Container } from './Layout.styled';
 
 export const Layout = () => {
   return (
-    <>
+    <Container>
+      <Bars />
       <AppBar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </>
+    </Container>
   );
 };

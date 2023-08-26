879db019b74a2aeb738ff045bf50f31cb6bb6d2e
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-08-26 21:44:25 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Sidebar }  from '../Sidebar';
+import { Sidebar }  from '../Sidebar/Sidebar';
 import { AppBar } from 'components/AppBar';
 import { Container } from './Layout.styled';
 

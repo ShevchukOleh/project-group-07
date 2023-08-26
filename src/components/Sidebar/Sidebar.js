a162bcfc6030f4b44b764a408399864c3362cfca
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-08-26 21:22:07 +0300

    bugs fixed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import logo from '../../images/logo.png';
 import plant from '../../images/cup.png';
-// import circle from '../../images/help-circle.png';
-import { BordInSidebar } from './BordInSidebar';
+import circle from '../../images/help-circle.png';
 import {
-  Block,
   HelpBlock,
   LogOutBlock,
   HelpText,
@@ -18,26 +16,25 @@ import {
   LogoImage,
   LogoContainer,
   SidebarContainer,
-  CustomDrawer,
-  BlockContainerBoard,
+  ContainerAside,
 } from './Sidebar.styled';
 
-const Sidebar = () => {
-  const isBoard = true;
-
+export const Sidebar = () => {
   return (
-    <CustomDrawer variant="permanent" anchor="left">
+    <ContainerAside>
       <SidebarContainer>
-        <Block>
+        <div>
           <LogoContainer>
             <LogoImage src={logo} alt="Logo" />
-            <LogoText>Task Pro</LogoText>
+            <LogoText variant="h6">Task Pro</LogoText>
           </LogoContainer>
+
           <InputContainer>
-            <StyledInput> My board</StyledInput>
+            <StyledInput placeholder="My boards" fullWidth />
           </InputContainer>
-          <BlockContainerCreate marginBottom={40}>
-            <NewBoardText>
+
+          <BlockContainerCreate marginBottom={206}>
+            <NewBoardText variant="body2">
               Create <br />a new board
             </NewBoardText>
             <Button
@@ -49,64 +46,44 @@ const Sidebar = () => {
                 background: '#BEDBB0',
               }}
             >
-              <p style={{ color: 'black', fontSize: '26px', margin: '0' }}>+</p>
+              +
             </Button>
           </BlockContainerCreate>
-        </Block>
-        {isBoard ? (
-          <BlockContainerBoard>
-            <BordInSidebar />
-          </BlockContainerBoard>
-        ) : (
-          <></>
-        )}
-        <Block>
+        </div>
+
+        <div>
           <BlockContainer marginBottom={24}>
             <img
               src={plant}
               alt="plant"
-              style={{ width: '54px', height: '78px', marginBottom: '14px' }}
+              style={{ width: '54px', height: '78px' }}
             />
+
             <HelpText variant="body2">
-              If you need help with
-              <a
-                href="http://www.google.com"
-                style={{ color: '#BEDBB0', textDecoration: 'none' }}
-              >
-                TaskPro
-              </a>
-              , check out our support resources or reach out to our customer
+              If you need help with <a href="http://www.google.com">TaskPro</a>,
+              check out our support resources or reach out to our customer
               support team.
+              <HelpBlock>
+                <img
+                  src={circle}
+                  alt="help"
+                  style={{ width: '20px', height: '20px', marginRight: '8px' }}
+                />
+
+                <p> Need help?</p>
+              </HelpBlock>
             </HelpText>
-            <HelpBlock>
-              <button
-                style={{
-                  border: 'none',
-                  display: 'flex',
-                  alignItems: 'center',
-                  background: 'inherit',
-                  padding: '0',
-                }}
-              >
-                <svg width={20} height={20} style={{ marginRight: '8px' }}>
-                  <use
-                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#icon-help-circle`}
-                  />
-                </svg>
-                <p style={{ margin: '0' }}> Need help?</p>
-              </button>
-            </HelpBlock>
           </BlockContainer>
+
           <LogOutBlock>
             <svg width={32} height={32} style={{ marginRight: '14px' }}>
               <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#logout`} />
             </svg>
             Log Out
           </LogOutBlock>
-        </Block>
+        </div>
       </SidebarContainer>
-    </CustomDrawer>
+    </ContainerAside> 
+    
   );
 };
-
-export default Sidebar;

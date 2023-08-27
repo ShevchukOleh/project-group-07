56b3e12a7107963685d73b0a42821be49ba591eb
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-27 18:33:03 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,41 +1,14 @@
-import { useState } from 'react';
+import { ThemeModal } from 'components/ThemeModal';
 import {
   Header,
   MenuBtn,
   MenuIconNav,
   HeaderNav,
-  ThemeBtn,
-  ThemeIcon,
   UserInfoBtn,
   UserAvatar,
 } from './AppBar.styled';
-import Icon from '../../images/symbol-defs.svg';
-import {
-  Menu,
-  Radio,
-  RadioGroup,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-} from '@mui/material';
 
 export const AppBar = () => {
-  const [themeValue, setThemeValue] = useState('');
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleChange = event => {
-    setThemeValue(event.currentTarget.value);
-  };
-  
   return (
     <Header>
       <MenuBtn>
@@ -43,47 +16,7 @@ export const AppBar = () => {
       </MenuBtn>
 
       <HeaderNav>
-        <ThemeBtn
-          id="theme-button"
-          aria-controls={open ? 'theme-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-        >
-          Theme
-          <ThemeIcon>
-            <use href={`${Icon}#icon-chevron-down`}></use>
-          </ThemeIcon>
-        </ThemeBtn>
-
-        <Menu
-          id="theme-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-        >
-          <FormControl>
-            <FormLabel id="radio-buttons-group-label"></FormLabel>
-            <RadioGroup
-              aria-labelledby="radio-buttons-group-label"
-              name="radio-buttons-group"
-              value={themeValue}
-              onChange={handleChange}
-            >
-              <FormControlLabel
-                value="light"
-                control={<Radio />}
-                label="Light"
-              />
-              <FormControlLabel value="dark" control={<Radio />} label="Dark" />
-              <FormControlLabel
-                value="violet"
-                control={<Radio />}
-                label="Violet"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Menu>
+        <ThemeModal />
 
         <UserInfoBtn>
           User

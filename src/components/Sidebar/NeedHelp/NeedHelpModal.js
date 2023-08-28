62df77fc523979126a3281c392ce3c982999f070
Ling Igor <lingigor1990@gmail.com>
2023-08-28 23:18:25 +0300

    commit needhelpModal

---
+++ src/components/Sidebar/NeedHelp/NeedHelpModal.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import {
+  StyledTextArea,
+  StyledInput,
+  ModalButton,
+  ModalTitle,
+  ModalContent,
+  ModalOverlay,
+  IconClose,
+} from './NeedHelpModal.styled';
+
+const ModalForm = ({ isModalOpen, closeModalAdnBackdrop }) => {
+  const initialValues = {
+    email: '',
+    comment: '',
+  };
+
+  const handleSubmit = values => {
+    if (values.email && values.comment) {
+      closeModalAdnBackdrop();
+      console.log(values);
+    } else {
+      alert('Sorry');
+    }
+  };
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      closeModalAdnBackdrop();
+    }
+  };
+
+  return (
+    <div>
+      {isModalOpen && (
+        <ModalOverlay onClick={handleBackdropClick}>
+          <ModalContent>
+            <ModalTitle>Need Help</ModalTitle>
+            <IconClose>
+              <use
+                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#icon-x-close`}
+                fill="black"
+              />
+            </IconClose>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+              <Form>
+                <StyledInput
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+                <StyledTextArea
+                  component="textarea"
+                  name="comment"
+                  placeholder="Comment"
+                  required
+                />
+                <ModalButton type="submit">Send</ModalButton>
+              </Form>
+            </Formik>
+          </ModalContent>
+        </ModalOverlay>
+      )}
+    </div>
+  );
+};
+
+export default ModalForm;

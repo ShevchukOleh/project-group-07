1cf8a0d8e6b1aa45281790b643fd6c0341cfd57f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-29 00:09:07 +0300

    Update FiltersModal.jsx

--- src/components/FiltersModal/FiltersModal.jsx
+++ src/components/FiltersModal/FiltersModal.jsx
@@ -10,12 +10,64 @@ import {
 import { FiltersBtn, FilterIcon } from './FiltersModal.styled';
 
 export const FiltersModal = () => {
+  const [filtersEl, setFiltersEl] = useState(null);
+  const open = Boolean(filtersEl);
+
+  const handleClick = event => {
+    setFiltersEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setFiltersEl(null);
+  };
+
   return (
     <>
-      <FiltersBtn>
+      <FiltersBtn
+        id="filters-button"
+        aria-controls={open ? 'theme-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
         <FilterIcon />
         Filters
       </FiltersBtn>
+
+      <Menu
+        id="filters-menu"
+        anchorEl={filtersEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <FormControl
+          sx={{
+            width: 300,
+            height: 234,
+            justifyContent: 'center',
+          }}
+        >
+          <FormLabel id="filters-radio-buttons-group-label">Filters</FormLabel>
+          <RadioGroup
+            aria-labelledby="filters-radio-buttons-group-label"
+            defaultValue="without priority"
+            name="filters-radio-buttons-group"
+          >
+            <FormControlLabel
+              value="without priority"
+              control={<Radio />}
+              label="Without priority"
+            />
+            <FormControlLabel value="low" control={<Radio />} label="Low" />
+            <FormControlLabel
+              value="medium"
+              control={<Radio />}
+              label="Medium"
+            />
+            <FormControlLabel value="high" control={<Radio />} label="High" />
+          </RadioGroup>
+        </FormControl>
+      </Menu>
     </>
   );
 };

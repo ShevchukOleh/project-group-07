738e43d318d2340b617966b59c9a3cfb2008392b
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-08-29 18:43:17 +0300

    add Loader in auth

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import { LoaderWrapper, Loader } from './Loader.style';
+
+const LoaderComponent = () => {
+  return (
+    <LoaderWrapper>
+      <Loader />
+    </LoaderWrapper>
+  );
+};
+export default LoaderComponent;

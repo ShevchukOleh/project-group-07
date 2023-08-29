a22b471833781435556a3957c344a0d34d04ad34
Rew <irinavakulenko2000@gmail.com>
2023-08-29 14:15:57 +0300

    Update servises.js

--- src/components/ModalBoard/servises.js
+++ src/components/ModalBoard/servises.js
@@ -1,3 +1,3 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const BASE_URL = 'https://taskpro-backend-zulp.onrender.com/api/board/background';
+// const BASE_URL = 'https://taskpro-backend-zulp.onrender.com/api/board/background';

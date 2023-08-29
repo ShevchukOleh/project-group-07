98f7733e61951c17e8862b7af9476ff4c61820c4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-29 18:25:06 +0300

    Update FiltersModal.styled.jsx

--- src/components/FiltersModal/FiltersModal.styled.jsx
+++ src/components/FiltersModal/FiltersModal.styled.jsx
@@ -18,3 +18,38 @@ export const FilterIcon = styled(FilterAltOutlinedIcon)`
   height: 16px;
   fill: currentColor;
 `;
+
+export const MenuLabel = styled.span`
+  display: block;
+  margin-bottom: 10px;
+  font-family: Poppins;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 0.02em;
+`;
+
+export const Border = styled.div`
+  display: block;
+  height: 1px;
+  margin-bottom: 10px;
+  background-color: rgba(22, 22, 22, 0.1);
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const ShowAllBtn = styled(Button)`
+  display: flex;
+  height: 18px;
+  padding: 0;
+  text-decoration: underline;
+  color: rgba(22, 22, 22, 0.5);
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 1.5;
+  text-transform: none;
+  letter-spacing: 0.02em;
+`;

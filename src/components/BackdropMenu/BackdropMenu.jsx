8478235bbd2b5540aaefa88a8b299860b49fc28a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-30 20:53:01 +0300

    Create BackdropMenu.jsx

---
+++ src/components/BackdropMenu/BackdropMenu.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import { Sidebar } from 'components/Sidebar/Sidebar';
+import { MenuBtn, MenuIconNav, Wrapper } from './BackdropMenu.styled';
+
+export const BackdropMenu = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <Wrapper>
+      <MenuBtn onClick={handleOpen}>
+        <MenuIconNav />
+      </MenuBtn>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <Sidebar />
+      </Backdrop>
+    </Wrapper>
+  );
+};

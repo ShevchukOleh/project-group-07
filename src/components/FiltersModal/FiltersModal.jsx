0b77667e9904ac4912acd0a5fecf53cf0a1d0708
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-31 21:53:14 +0300

    Update FiltersModal.jsx

--- src/components/FiltersModal/FiltersModal.jsx
+++ src/components/FiltersModal/FiltersModal.jsx
@@ -17,6 +17,7 @@ import {
   Wrapper,
   ShowAllBtn,
   MenuLabelWrap,
+  MenuWrap,
 } from './FiltersModal.styled';
 
 export const FiltersModal = () => {
@@ -104,165 +105,170 @@ export const FiltersModal = () => {
           horizontal: 'right',
         }}
         sx={{
-          '& .css-3dzjca-MuiPaper-root-MuiPopover-paper-MuiMenu-paper': {
-            padding: '26px 24px',
-            borderRadius: '8px',
-            boxShadow: '0px 4px 16px 0px rgba(22, 22, 22, 0.05)',
-            backgroundColor: '#FCFCFC',
-          },
           '& ul': {
             padding: 0,
           },
-          '& ul > span': { color: '#161616' },
+          '& ul > div': {
+            boxShadow: '0px 4px 16px 0px rgba(22, 22, 22, 0.05)',
+            backgroundColor: '#FCFCFC',
+            borderRadius: '8px',
+          },
+          '& ul > div > span': { color: '#161616' },
         }}
       >
-        <MenuLabelWrap>
-          <MenuLabel>Filters</MenuLabel>
+        <MenuWrap>
+          <MenuLabelWrap>
+            <MenuLabel>Filters</MenuLabel>
 
-          <Button
-            onClick={handleClose}
-            sx={{
-              padding: 0,
-              minWidth: '18px',
-              height: '18px',
-              position: 'absolute',
-              top: '-15px',
-              right: '-15px',
-            }}
-          >
-            <CloseOutlinedIcon
+            <Button
+              onClick={handleClose}
               sx={{
+                padding: 0,
                 minWidth: '18px',
                 height: '18px',
-                fill: 'rgba(22, 22, 22, 1)',
-              }}
-            />
-          </Button>
-        </MenuLabelWrap>
-
-        <Border />
-
-        <Wrapper>
-          <FormControl
-            sx={{
-              width: 188,
-              justifyContent: 'center',
-            }}
-          >
-            <FormLabel
-              id="filters-radio-buttons-group-label"
-              sx={{
-                marginBottom: '10px',
-                color: 'rgba(22, 22, 22, 1)',
-                fontFamily: 'Poppins',
-                fontWeight: 500,
-                fontSize: '14px',
-                lineHeight: 1.5,
-                letterSpacing: '0.02em',
+                position: 'absolute',
+                top: '10px',
+                right: '8px',
               }}
             >
-              Label color
-            </FormLabel>
+              <CloseOutlinedIcon
+                sx={{
+                  minWidth: '18px',
+                  height: '18px',
+                  fill: 'rgba(22, 22, 22, 1)',
+                }}
+              />
+            </Button>
+          </MenuLabelWrap>
 
-            <RadioGroup
-              aria-labelledby="filters-radio-buttons-group-label"
-              name="filters-radio-buttons-group"
-              value={filterValue}
-              onChange={handleChange}
+          <Border />
+
+          <Wrapper>
+            <FormControl
               sx={{
-                '&': { gap: '8px' },
-                '& label': { margin: 0, gap: '8px' },
-                '& label .MuiTypography-root': {
+                width: 188,
+                justifyContent: 'center',
+              }}
+            >
+              <FormLabel
+                id="filters-radio-buttons-group-label"
+                sx={{
+                  marginBottom: '10px',
+                  color: 'rgba(22, 22, 22, 1)',
                   fontFamily: 'Poppins',
-                  fontWeight: 400,
-                  fontSize: '12px',
+                  fontWeight: 500,
+                  fontSize: '14px',
                   lineHeight: 1.5,
                   letterSpacing: '0.02em',
-                },
-                '& label .MuiButtonBase-root': { padding: 0 },
-                '& label > span span svg': { width: '14px', height: '14px' },
-              }}
-            >
-              <FormControlLabel
-                value="without priority"
-                control={
-                  <Radio
-                    sx={{
-                      backgroundColor: bgPriorityColor(
-                        withoutStatus,
-                        'rgba(22, 22, 22, 0.3)'
-                      ),
-                      '& span svg': { color: 'rgba(22, 22, 22, 0.3)' },
-                    }}
-                  />
-                }
-                label="Without priority"
+                }}
+              >
+                Label color
+              </FormLabel>
+
+              <RadioGroup
+                aria-labelledby="filters-radio-buttons-group-label"
+                name="filters-radio-buttons-group"
+                value={filterValue}
+                onChange={handleChange}
                 sx={{
-                  '& .MuiTypography-root': {
-                    color: priorityColor(withoutStatus),
+                  '&': { gap: '8px' },
+                  '& label': { margin: 0, gap: '8px' },
+                  '& label .MuiTypography-root': {
+                    fontFamily: 'Poppins',
+                    fontWeight: 400,
+                    fontSize: '12px',
+                    lineHeight: 1.5,
+                    letterSpacing: '0.02em',
                   },
+                  '& label .MuiButtonBase-root': { padding: 0 },
+                  '& label > span span svg': { width: '14px', height: '14px' },
                 }}
-              />
-              <FormControlLabel
-                value="low"
-                control={
-                  <Radio
-                    sx={{
-                      backgroundColor: bgPriorityColor(
-                        lowStatus,
-                        'rgba(143, 161, 208, 1)'
-                      ),
-                      '& span svg': { color: 'rgba(143, 161, 208, 1)' },
-                    }}
-                  />
-                }
-                label="Low"
-                sx={{
-                  '& .MuiTypography-root': { color: priorityColor(lowStatus) },
-                }}
-              />
-              <FormControlLabel
-                value="medium"
-                control={
-                  <Radio
-                    sx={{
-                      backgroundColor: bgPriorityColor(
-                        mediumStatus,
-                        'rgba(224, 156, 181, 1)'
-                      ),
-                      '& span svg': { color: 'rgba(224, 156, 181, 1)' },
-                    }}
-                  />
-                }
-                label="Medium"
-                sx={{
-                  '& .MuiTypography-root': {
-                    color: priorityColor(mediumStatus),
-                  },
-                }}
-              />
-              <FormControlLabel
-                value="high"
-                control={
-                  <Radio
-                    sx={{
-                      backgroundColor: bgPriorityColor(
-                        highStatus,
-                        'rgba(190, 219, 176, 1)'
-                      ),
-                      '& span svg': { color: 'rgba(190, 219, 176, 1)' },
-                    }}
-                  />
-                }
-                label="High"
-                sx={{
-                  '& .MuiTypography-root': { color: priorityColor(highStatus) },
-                }}
-              />
-            </RadioGroup>
-          </FormControl>
-          <ShowAllBtn onClick={handleShowAllBtnClick}>Show all</ShowAllBtn>
-        </Wrapper>
+              >
+                <FormControlLabel
+                  value="without priority"
+                  control={
+                    <Radio
+                      sx={{
+                        backgroundColor: bgPriorityColor(
+                          withoutStatus,
+                          'rgba(22, 22, 22, 0.3)'
+                        ),
+                        '& span svg': { color: 'rgba(22, 22, 22, 0.3)' },
+                      }}
+                    />
+                  }
+                  label="Without priority"
+                  sx={{
+                    '& .MuiTypography-root': {
+                      color: priorityColor(withoutStatus),
+                    },
+                  }}
+                />
+                <FormControlLabel
+                  value="low"
+                  control={
+                    <Radio
+                      sx={{
+                        backgroundColor: bgPriorityColor(
+                          lowStatus,
+                          'rgba(143, 161, 208, 1)'
+                        ),
+                        '& span svg': { color: 'rgba(143, 161, 208, 1)' },
+                      }}
+                    />
+                  }
+                  label="Low"
+                  sx={{
+                    '& .MuiTypography-root': {
+                      color: priorityColor(lowStatus),
+                    },
+                  }}
+                />
+                <FormControlLabel
+                  value="medium"
+                  control={
+                    <Radio
+                      sx={{
+                        backgroundColor: bgPriorityColor(
+                          mediumStatus,
+                          'rgba(224, 156, 181, 1)'
+                        ),
+                        '& span svg': { color: 'rgba(224, 156, 181, 1)' },
+                      }}
+                    />
+                  }
+                  label="Medium"
+                  sx={{
+                    '& .MuiTypography-root': {
+                      color: priorityColor(mediumStatus),
+                    },
+                  }}
+                />
+                <FormControlLabel
+                  value="high"
+                  control={
+                    <Radio
+                      sx={{
+                        backgroundColor: bgPriorityColor(
+                          highStatus,
+                          'rgba(190, 219, 176, 1)'
+                        ),
+                        '& span svg': { color: 'rgba(190, 219, 176, 1)' },
+                      }}
+                    />
+                  }
+                  label="High"
+                  sx={{
+                    '& .MuiTypography-root': {
+                      color: priorityColor(highStatus),
+                    },
+                  }}
+                />
+              </RadioGroup>
+            </FormControl>
+            <ShowAllBtn onClick={handleShowAllBtnClick}>Show all</ShowAllBtn>
+          </Wrapper>
+        </MenuWrap>
       </Menu>
     </>
   );

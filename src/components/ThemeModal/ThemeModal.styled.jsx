350843ffd5b37db02ed4003c91b642eeed43439f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-31 21:53:42 +0300

    Update ThemeModal.styled.jsx

--- src/components/ThemeModal/ThemeModal.styled.jsx
+++ src/components/ThemeModal/ThemeModal.styled.jsx
@@ -1,17 +1,31 @@
 import styled from '@emotion/styled';
 import { Button } from '@mui/material';
 
+//const currentTheme = 'light';
 export const ThemeBtn = styled(Button)`
   gap: 4px;
   padding-top: 5px;
   padding-bottom: 5px;
-  color: #161616;
+  color: ${props => {
+    switch (props.currentTheme) {
+      case 'light':
+        return props.theme?.palette?.primary?.main;
+      case 'dark':
+        return props.theme?.palette?.secondary?.main;
+      default: return '#000';
+    }
+  }};
   font-family: Poppins;
   font-size: 14px;
   font-weight: 500;
   line-height: 1.5;
   text-transform: none;
   letter-spacing: 0.02em;
+  ${props => {
+  console.log(props.theme?.palette?.primary?.main);
+  console.log(props.theme?.palette?.secondary?.main);
+  console.log(props.currenttheme);
+  }}
 `;
 
 export const ThemeIcon = styled.svg`

2327904a7e12a6b2a5387996eaab810dc83dde74
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-31 21:53:49 +0300

    Update theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,1 +1,14 @@
-export const theme = {};
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#161616',
+      second: '',
+    },
+    secondary: {
+      main: '#fff',
+      second: '',
+    },
+  },
+});

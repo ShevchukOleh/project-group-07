9f01bb48b0106ad3b2cbbc32338bb35d0b604037
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-31 16:32:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
-
+import { ThemeProvider } from '@mui/material';
+import { theme } from './constants';
+console.log(theme)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <BrowserRouter basename="/project-group-07">
         <React.StrictMode>
-          <App />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </React.StrictMode>
       </BrowserRouter>
     </PersistGate>

dd6c1ce0f26a87dd87a8b2dcb6243e73c657d770
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-08-31 11:55:14 +0300

    Update board.js

--- src/store/createSlices/board/board.js
+++ src/store/createSlices/board/board.js
@@ -4,6 +4,7 @@ import {
   createBoard,
   getAllColums,
   createColumn,
+  deleteBoard,
 } from 'store/AsyncThunk/asyncThunkBoards';
 
 const initialState = {
@@ -70,17 +71,18 @@ const boardSlice = createSlice({
       .addCase(createColumn.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
-      // .addCase(deleteBoard.pending, (state, action) => {
-      //   state.loading = true;
-      // })
-      // .addCase(deleteBoard.fulfilled, (state, action) => {
-      //   state.loading = false;
-      //   state.error = null;
-      //   state.boards = state.boards.filter(el => el.id !== action.payload.id);
-      // })
-      // .addCase(deleteBoard.rejected, (state, action) => {
-      //   state.loading = false;
-      //   state.error = action.payload;
+      })
+      .addCase(deleteBoard.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(deleteBoard.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.boards = state.boards.filter(el => el.id !== action.payload.id);
+      })
+      .addCase(deleteBoard.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       });
   },
 });

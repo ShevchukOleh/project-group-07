5decbfc4615fcbaf6baf581fe92f09dbfc3c0dd7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-01 00:10:58 +0300

    Update theme.js

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,14 +1,37 @@
 import { createTheme } from '@mui/material';
 
+const currentTheme = 'light';
+
+const lightTheme = {
+  first: 'rgba(22, 22, 22, 0.8)',
+  second: '#161616',
+  third: 'rgba(190,219,176,1)',
+};
+
+const darkTheme = {
+  first: '#fff',
+  second: '#fcfcfc',
+};
+
+const coloredTheme = { first: '#ececec', second: '#DBBEb0' };
+
+const getTheme = theme => {
+  switch (theme) {
+    case 'light':
+      return lightTheme;
+    case 'dark':
+      return darkTheme;
+    case 'colored':
+      return coloredTheme;
+    default:
+      return lightTheme;
+  }
+};
+
+const themeSet = getTheme(currentTheme);
+
 export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#161616',
-      second: '',
-    },
-    secondary: {
-      main: '#fff',
-      second: '',
-    },
-  },
+  themeSet,
 });
+
+console.log(theme);

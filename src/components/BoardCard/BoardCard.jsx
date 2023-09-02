5d86d884ec5053064c8f9525cb38abeb859af2a3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-02 18:56:10 +0300

    Update BoardCard.jsx

--- src/components/BoardCard/BoardCard.jsx
+++ src/components/BoardCard/BoardCard.jsx
@@ -11,9 +11,11 @@ export default function BoardCard() {
   const selectPriority = useSelector(selectedInPriority);
   const selectCards = useSelector(selectMyCards);
   const [cards, setCards] = useState([]);
+
   useEffect(() => {
     setCards(selectCards);
   }, [selectPriority, selectCards]);
+
   return (
     <>
       {cards.map((item, index) => (
@@ -27,30 +29,39 @@ function BoardCardItem({ card }) {
   return (
     <BoardCardStyle>
       <h3 className="title">{card.title}</h3>
+
       <p className="description clip">{card.description}</p>
       <hr />
+
       <div style={{ display: 'flex', alignItems: 'flex-start', gap: '14px' }}>
         <div>
           <span className="priorityTitle">Priority</span>
+
           <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
             <span className="priorityColor"></span>
+
             <span className="priorityText">{card.priority}</span>
           </div>
         </div>
+
         <div>
           <span className="priorityTitle">Deadline</span>
+
           <div>
             <span className="priorityText">{card.deadline}</span>
           </div>
         </div>
       </div>
+
       <div className="containerCardIcon">
         <FiArrowRightCircle
           style={{ cursor: 'pointer', color: 'rgba(16, 16, 16, 0.5)' }}
         />
+
         <FiEdit2
           style={{ cursor: 'pointer', color: 'rgba(16, 16, 16, 0.5)' }}
         />
+        
         <FiTrash
           style={{ cursor: 'pointer', color: 'rgba(16, 16, 16, 0.5)' }}
         />

0d6a22e49c3595e80b6c3ddfc9114aed007bafbd
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-03 17:49:57 +0300

    Update AddColumn.jsx

--- src/components/Modals/AddColumn/AddColumn.jsx
+++ src/components/Modals/AddColumn/AddColumn.jsx
@@ -8,21 +8,26 @@ import {
   IconPlusWrapper,
 } from '../UI/ModalCulumn.styled';
 import { AiOutlinePlus } from 'react-icons/ai';
+
 const AddColumn = () => {
   const [addColumn, setAddColumn] = useState('');
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log(addColumn);
   };
+
   return (
     <ColumnModalContainer>
       <ColumnModalTitle>Add column</ColumnModalTitle>
+
       <ColumnModalForm>
         <ColumnModalFormInput
           placeholder="To Do"
           value={addColumn}
           onChange={e => setAddColumn(e.target.value)}
         />
+
         <ColumnModalFormBtn onClick={handleSubmit}>
           <IconPlusWrapper>
             <AiOutlinePlus size={16} color="white" />
@@ -33,4 +38,5 @@ const AddColumn = () => {
     </ColumnModalContainer>
   );
 };
+
 export default AddColumn;

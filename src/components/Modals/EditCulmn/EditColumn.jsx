6b49097b9faa077202d167dc03c3ac2daa806e26
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-03 17:50:04 +0300

    Update EditColumn.jsx

--- src/components/Modals/EditCulmn/EditColumn.jsx
+++ src/components/Modals/EditCulmn/EditColumn.jsx
@@ -8,21 +8,26 @@ import {
   IconPlusWrapper,
 } from '../UI/ModalCulumn.styled';
 import { useState } from 'react';
+
 const EditColumn = () => {
   const [change, setChange] = useState('');
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log(change);
   };
+
   return (
     <ColumnModalContainer>
       <ColumnModalTitle>Edit column</ColumnModalTitle>
+
       <ColumnModalForm>
         <ColumnModalFormInput
           placeholder="To Do"
           value={change}
           onChange={e => setChange(e.target.value)}
         />
+
         <ColumnModalFormBtn onClick={handleSubmit}>
           <IconPlusWrapper>
             <AiOutlinePlus size={16} color="white" />
@@ -33,4 +38,5 @@ const EditColumn = () => {
     </ColumnModalContainer>
   );
 };
+
 export default EditColumn;

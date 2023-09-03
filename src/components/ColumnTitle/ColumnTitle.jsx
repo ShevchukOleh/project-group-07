651f50be70086cf57edc27a911c1546605e43f86
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-03 17:40:25 +0300

    Update ColumnTitle.jsx

--- src/components/ColumnTitle/ColumnTitle.jsx
+++ src/components/ColumnTitle/ColumnTitle.jsx
@@ -3,6 +3,7 @@ import { ColumnTitleStyle } from './ColumnTitle.styled';
 import { FiTrash, FiEdit2 } from 'react-icons/fi';
 import { useDispatch } from 'react-redux';
 import { deleteColumn } from 'store/AsyncThunk/asyncThunkBoards';
+import { theme } from '../../constants';
 
 export default function ColumnTitle(params) {
   const dispatch = useDispatch();
@@ -25,12 +26,18 @@ export default function ColumnTitle(params) {
         }}
       >
         <FiEdit2
-          style={{ cursor: 'pointer', color: 'rgba(16, 16, 16, 0.5)' }}
+          style={{
+            cursor: 'pointer',
+            color: `${theme?.themeSet?.cardPriorityIcon}`,
+          }}
         />
 
         <FiTrash
           onClick={() => dispatch(deleteColumn(requestData))}
-          style={{ cursor: 'pointer', color: 'rgba(16, 16, 16, 0.5)' }}
+          style={{
+            cursor: 'pointer',
+            color: `${theme?.themeSet?.cardPriorityIcon}`,
+          }}
         />
       </div>
     </ColumnTitleStyle>

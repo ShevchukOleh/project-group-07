b193f63810ea68fb4e28554408e21e5537cb5922
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-03 17:32:26 +0300

    Update ColumnTitle.styled.jsx

--- src/components/ColumnTitle/ColumnTitle.styled.jsx
+++ src/components/ColumnTitle/ColumnTitle.styled.jsx
@@ -5,14 +5,20 @@ export const ColumnTitleStyle = styled.div`
   width: 334px;
   margin-bottom: 8px;
   padding: 18px 60px 17px 20px;
-  background-color: #ffffff;
+  background-color: ${props => {
+    return props?.theme?.themeSet?.cardBg;
+  }};
   border-radius: 8px;
+
   & p {
     margin: 0;
     padding: 0;
   }
+
   & .titleColumn {
-    color: #161616;
+    color: ${props => {
+      return props?.theme?.themeSet?.boardTitle;
+    }};
     font-size: 14px;
     font-weight: 500;
     letter-spacing: -0.28px;

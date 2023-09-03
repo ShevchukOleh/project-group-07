9dde93dcb0acbaafbf62001786f3ae45ea68a7a1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-03 19:18:53 +0300

    Update ModalCulumn.styled.jsx

--- src/components/Modals/UI/ModalCulumn.styled.jsx
+++ src/components/Modals/UI/ModalCulumn.styled.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const ColumnModalContainer = styled.div`
   height: 221px;
@@ -8,9 +9,10 @@ const ColumnModalContainer = styled.div`
   width: 335px;
   padding: 24px;
   border-radius: 8px;
-  border: 1px solid ${props => {
-    return props?.theme?.themeSet?.modalHelpBorder;
-  }};
+  border: 1px solid
+    ${props => {
+      return props?.theme?.themeSet?.modalHelpBorder;
+    }};
 
   @media screen and (min-width: 480px) {
     width: 350px;
@@ -37,14 +39,35 @@ const ColumnModalForm = styled.form`
 
 const ColumnModalFormInput = styled.input`
   border-radius: 8px;
-  border: 1px solid #bedbb0;
+  border: 1px solid
+    ${props => {
+      return props?.theme?.themeSet?.inputOtherBorder;
+    }};
+  outline: none;
   opacity: 0.4;
   width: 100%;
-  color: #fff;
+  color: ${props => {
+    return props?.theme?.themeSet?.inputColor;
+  }};
   padding: 14px 0 14px 18px;
-  background-color: #1f1f1f;
+  background-color: ${props => {
+    return props?.theme?.themeSet?.inputBgColor;
+  }};
   box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
   margin-bottom: 24px;
+
+  ::placeholder {
+    color: ${props => {
+      return props?.theme?.themeSet?.inputPlaceholder;
+    }};
+  }
+
+  &:hover {
+    border: 1px solid
+      ${props => {
+        return props?.theme?.themeSet?.inputOtherHover;
+      }};
+  }
 `;
 
 const ColumnModalFormBtn = styled.button`
@@ -52,20 +75,64 @@ const ColumnModalFormBtn = styled.button`
   padding: 10px 0px 10px 0px;
   justify-content: center;
   align-items: center;
-  border-radius: 8px;
+  border-radius: 4px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${props => {
+    return props?.theme?.themeSet?.modalHelpSendBorder;
+  }};
   gap: 5px;
-  background: #bedbb0;
+  background-color: ${props => {
+    return props?.theme?.themeSet?.modalHelpSendBg;
+  }};
+  color: ${props => {
+    return props?.theme?.themeSet?.modalHelpSendText;
+  }};
   width: 100%;
+  transition: color, background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    color: ${props => {
+      return props?.theme?.themeSet?.modalHelpSendHoverText;
+    }};
+    background-color: ${props => {
+      return props?.theme?.themeSet?.modalHelpSendHover;
+    }};
+  }
+
+  &:hover > div {
+    background-color: ${props => {
+      return props?.theme?.themeSet?.boxIconBgHover;
+    }};
+  }
+
+  &:hover > div > svg {
+    fill: ${props => {
+      return props?.theme?.themeSet?.iconFillHover;
+    }};
+  }
 `;
 
 const IconPlusWrapper = styled.div`
   width: 28px;
   height: 28px;
-  background-color: #161616;
+  background-color: ${props => {
+    return props?.theme?.themeSet?.boxIconBg;
+  }};
   border-radius: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`;
+
+const IconPlus = styled(AiOutlinePlus)`
+  width: 18px;
+  height: 18px;
+  transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  fill: ${props => {
+    return props?.theme?.themeSet?.iconFill;
+  }};
 `;
 
 export {
@@ -75,4 +142,5 @@ export {
   ColumnModalFormInput,
   ColumnModalFormBtn,
   IconPlusWrapper,
+  IconPlus,
 };

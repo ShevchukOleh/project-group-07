333a15244196bff789126dc9fa30c447e9da8efc
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-04 23:24:32 +0300

    Update ButtonCreate.styled.jsx

--- src/components/ButtonCreate/ButtonCreate.styled.jsx
+++ src/components/ButtonCreate/ButtonCreate.styled.jsx
@@ -4,23 +4,11 @@ export const ButtonCreateStyle = styled.button`
   width: 335px;
   border-radius: 8px;
   background-color: ${props => {
-    return props?.theme?.themeSet?.modalHelpSendBg;
+    return props?.theme?.themeSet?.addColumnBtnBg;
   }};
   padding: 14px 78px 14px 79px;
-  outline: none;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${props => {
-    return props?.theme?.themeSet?.modalHelpSendBorder;
-  }};
+  border: none;
   cursor: pointer;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  &:hover {
-    background-color: ${props => {
-      return props?.theme?.themeSet?.modalHelpSendHover;
-    }};
-  }
 
   & .containerLogoName {
     display: flex;
@@ -33,14 +21,14 @@ export const ButtonCreateStyle = styled.button`
     width: 28px;
     height: 28px;
     background-color: ${props => {
-      return props?.theme?.themeSet?.createBtnIconBg;
+      return props?.theme?.themeSet?.addColumnIconBox;
     }};
     border-radius: 8px;
   }
 
   & .iconBtn {
     fill: ${props => {
-      return props?.theme?.themeSet?.createBtnIcon;
+      return props?.theme?.themeSet?.addColumnIcon;
     }};
     width: 14px;
     height: 14px;
@@ -54,7 +42,7 @@ export const ButtonCreateStyle = styled.button`
     line-height: normal;
     letter-spacing: -0.28px;
     color: ${props => {
-      return props?.theme?.themeSet?.modalHelpSendText;
+      return props?.theme?.themeSet?.addColumnText;
     }};
   }
 `;

55bcdccf91eba0688c3824a2a9413b58c1f4d2a1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-05 17:10:29 +0300

    Update ModalEditProfile.jsx

--- src/components/Modals/ModalEditProfile/ModalEditProfile.jsx
+++ src/components/Modals/ModalEditProfile/ModalEditProfile.jsx
@@ -5,61 +5,70 @@ import {
   IconClose,
 } from '../UI/ModalCulumn.styled';
 import { Formik, Form, Field } from 'formik';
-import {ModalContent, ModalTitle } from '../NeedHelp/NeedHelpModal.styled';
+import { ModalContent, ModalTitle } from '../NeedHelp/NeedHelpModal.styled';
 import { useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
-import noPhotoUser from '../../../../src/images/noImage-user2x.jpg'
-import { AddAvatar, IconPlus, IconPlusAvatar, ModalButton } from './ModalEditProfile.styled';
-import { addUserAvatar, fetchCurrentUser } from 'store/AsyncThunk/asyncThunkUsersAuth';
+import noPhotoUser from '../../../../src/images/noImage-user2x.jpg';
+import {
+  AddAvatar,
+  IconPlus,
+  IconPlusAvatar,
+  ModalButton,
+} from './ModalEditProfile.styled';
+import {
+  addUserAvatar,
+  fetchCurrentUser,
+} from 'store/AsyncThunk/asyncThunkUsersAuth';
 
 const ModalEditProfile = ({ createOpenModalShow, isCreateOpenModal, user }) => {
-    const [imageFile, setImageFile] = useState(null);
-    const [userAvatar, setUserAvatar] = useState(user?.avatarURL || noPhotoUser);
-    const dispatch = useDispatch();
-    const MAX_FILE_SIZE = 4 * 1024 * 1024;
+  const [imageFile, setImageFile] = useState(null);
+  const [userAvatar, setUserAvatar] = useState(user?.avatarURL || noPhotoUser);
+  const dispatch = useDispatch();
+  const MAX_FILE_SIZE = 4 * 1024 * 1024;
 
-    const handleFileUpload = (event) => {
-        const file = event.target.files[0];
+  const handleFileUpload = event => {
+    const file = event.target.files[0];
 
-        if (file && file.size <= MAX_FILE_SIZE) {
-            setImageFile(file);
-        } else {
-            console.error('File size exceeds the maximum limit of 4 megabytes.');
-            event.target.value = null;
-        }
-    };
-  
-    useEffect(() => {
-      if (imageFile) {
-        setUserAvatar(URL.createObjectURL(imageFile));
-      } else {
-        setUserAvatar(user?.avatarURL || noPhotoUser);
-      }
-    }, [imageFile, user?.avatarURL]);
-  
-    const initialValues = {
-      name: user?.name || '',
-      email: user?.email || '',
-      password: '',
-    };
-  
-    const onSubmit = async (values) => {
-      createOpenModalShow();
-      if (imageFile) {
-        await dispatch(addUserAvatar(imageFile));
-      } 
-      dispatch(fetchCurrentUser())
-    };
+    if (file && file.size <= MAX_FILE_SIZE) {
+      setImageFile(file);
+    } else {
+      console.error('File size exceeds the maximum limit of 4 megabytes.');
+      event.target.value = null;
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      setUserAvatar(URL.createObjectURL(imageFile));
+    } else {
+      setUserAvatar(user?.avatarURL || noPhotoUser);
+    }
+  }, [imageFile, user?.avatarURL]);
+
+  const initialValues = {
+    name: user?.name || '',
+    email: user?.email || '',
+    password: '',
+  };
+
+  const onSubmit = async values => {
+    createOpenModalShow();
+    if (imageFile) {
+      await dispatch(addUserAvatar(imageFile));
+    }
+    dispatch(fetchCurrentUser());
+  };
 
   const handleCloseModal = () => {
     createOpenModalShow(false);
   };
-    
+
   return (
     <div>
       <Dialog open={isCreateOpenModal} onClose={createOpenModalShow}>
         <ModalContent>
           <ModalTitle>Edit profile</ModalTitle>
+
           <Formik initialValues={initialValues} onSubmit={onSubmit}>
             <Form>
               <AddAvatar>
@@ -70,15 +79,19 @@ const ModalEditProfile = ({ createOpenModalShow, isCreateOpenModal, user }) => {
                   onChange={handleFileUpload}
                 />
                 <label htmlFor="file-input">
-                    <IconButton color="primary" component="span">
-                        <img src={userAvatar} alt='user-avatar' style={{ width: 68, height: 68 }} />
+                  <IconButton color="primary" component="span">
+                    <img
+                      src={userAvatar}
+                      alt="user-avatar"
+                      style={{ width: 68, height: 68, borderRadius: '8px' }}
+                    />
                     <IconPlusAvatar>
-                        <IconPlus/>
+                      <IconPlus />
                     </IconPlusAvatar>
                   </IconButton>
                 </label>
               </AddAvatar>
-                          
+
               <Field
                 type="text"
                 name="name"
@@ -100,6 +113,7 @@ const ModalEditProfile = ({ createOpenModalShow, isCreateOpenModal, user }) => {
               <ModalButton type="submit">Send</ModalButton>
             </Form>
           </Formik>
+
           <IconCrossWrapper onClick={handleCloseModal}>
             <IconClose />
           </IconCrossWrapper>

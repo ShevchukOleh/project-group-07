430a5ed9d30065e34113f7c7ef1ad60e08b5d7c3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-07 22:44:56 +0300

    Update CreateBtn.jsx

--- src/components/Buttons/CreateBtn/CreateBtn.jsx
+++ src/components/Buttons/CreateBtn/CreateBtn.jsx
@@ -1,7 +1,7 @@
-import { StyledCreateBtn, StyledAddIcon } from './CreateBtn.styled';
+import { StyledCreateBtn, StyledAddIconBig } from './CreateBtn.styled';
 
 export const CreateBtn = ({ onClick }) => (
   <StyledCreateBtn onClick={onClick}>
-    <StyledAddIcon />
+    <StyledAddIconBig />
   </StyledCreateBtn>
 );

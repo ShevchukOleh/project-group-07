12519618d8d778fe22b928428d60a4c790a3211e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-07 22:45:03 +0300

    Update CreateBtn.styled.jsx

--- src/components/Buttons/CreateBtn/CreateBtn.styled.jsx
+++ src/components/Buttons/CreateBtn/CreateBtn.styled.jsx
@@ -18,7 +18,7 @@ export const StyledCreateBtn = styled(Button)`
   }
 `;
 
-export const StyledAddIcon = styled(AddIcon)`
+export const StyledAddIconBig = styled(AddIcon)`
   width: 20px;
   height: 20px;
   fill: ${props => {
